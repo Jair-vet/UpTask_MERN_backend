bc7e108eba4b5c29bb0614a7d22fd03e2b56d4a7
Jair <jairaceves56@gmail.com>
2023-09-05 13:33:08 -0600

    Validamos si el correo es el mismo, no lo vuelva a crear

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,6 +2,15 @@ import Usuario from '../models/Usuario.js'
 
 
 const registrar = async (req, res) => {
+    // Evitar registros duplicados
+    const { email } = req.body
+    const existeUsuario = await Usuario.findOne({ email })
+
+    // Si existe mandamos el error
+    if (existeUsuario){
+        const error = new Error('El Usuario Ya Esta Registrado')
+        return res.status(400).json({ msg: error.message })
+    }
 
     try {
         const usuario = new Usuario(req.body)
