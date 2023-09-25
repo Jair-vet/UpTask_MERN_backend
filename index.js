e1ba122e90e4013660b888eebd8dea501b2d0bc1
Jair <jairaceves56@gmail.com>
2023-09-25 17:53:23 -0600

    Agregamos CORS para permitir la comunicacio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import cors from 'cors';
 import conectarDB from './config/db.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import proyectoRoutes from "./routes/proyectoRoutes.js";
@@ -12,6 +13,23 @@ app.use(express.json()) // para procesar la información JSON
 dotenv.config()
 
 conectarDB()
+
+// Configurar CORS
+const whiteList = ['http://localhost:5173']
+
+const corsOptions = {
+    origin: function(origin, callback){
+        if(whiteList.includes(origin)){
+            // Puede consultar API
+            callback(null, true)
+        }else{
+            // No esta permitido
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
 
 // Routing
 app.use('/api/usuarios', usuarioRoutes)
