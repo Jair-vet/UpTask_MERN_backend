faa90b3882321adecd168cea35d6d33fbfc96325
Jair <jairaceves56@gmail.com>
2023-09-25 18:08:22 -0600

    Agregamos una Variable de entorno

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config()
 conectarDB()
 
 // Configurar CORS
-const whiteList = ['http://localhost:5173']
+const whiteList = [process.env.FRONTEND_URL]
 
 const corsOptions = {
     origin: function(origin, callback){
