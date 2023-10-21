4d98d47d490d60d079a8eaeedf90bf7637c549bd
Jair <jairaceves56@gmail.com>
2023-10-20 20:01:56 -0600

    Agregamos un buscador en el Controller

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -4,51 +4,55 @@ import Usuario from "../models/Usuario.js";
 
 
 const obtenerProyectos = async (req, res) => {
-     // trae todos los proyectos en la DB que creo el usuario
-    const proyectos = await Proyecto.find().where('creador').equals(req.usuario).select("-tareas")
+    const proyectos = await Proyecto.find({
+      $or: [
+        { colaboradores: { $in: req.usuario } },
+        { creador: { $in: req.usuario } },
+      ],
+    }).select("-tareas");
+    res.json(proyectos);
+};
 
-    res.json(proyectos)
-
-}
 
 const nuevoProyecto = async (req, res) => {
-    
-    const proyecto = new Proyecto(req.body)
-    proyecto.creador = req.usuario._id
-    
+    const proyecto = new Proyecto(req.body);
+    proyecto.creador = req.usuario._id;
+  
     try {
-        const proyectoAlmacenado = await proyecto.save()
-        res.json(proyectoAlmacenado)
-    
+      const proyectoAlmacenado = await proyecto.save();
+      res.json(proyectoAlmacenado);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-
-}
+};
 
 const obtenerProyecto = async (req, res) => {
-
     const { id } = req.params;
-
-    const proyecto = await Proyecto.findById(id).populate('tareas')
-
+  
+    const proyecto = await Proyecto.findById(id)
+      .populate({
+        path: "tareas",
+        populate: { path: "completado", select: "nombre" },
+      })
+      .populate("colaboradores", "nombre email");
+  
     if (!proyecto) {
-        const error = new Error("Proyecto No Encontrado");
-        return res.status(404).json({ msg: error.message });
+      const error = new Error("No Encontrado");
+      return res.status(404).json({ msg: error.message });
     }
-
-    if (proyecto.creador.toString() !== req.usuario._id.toString() ) {
-        const error = new Error("Acción No Válida");
-        return res.status(401).json({ msg: error.message });
+  
+    if (
+      proyecto.creador.toString() !== req.usuario._id.toString() &&
+      !proyecto.colaboradores.some(
+        (colaborador) => colaborador._id.toString() === req.usuario._id.toString()
+      )
+    ) {
+      const error = new Error("Acción No Válida");
+      return res.status(401).json({ msg: error.message });
     }
-
-
-    // Obtener las tareas del Proyecto
-    // const tareas = await Tarea.find().where('proyecto').equals(proyecto._id)
-
-    res.json( proyecto );
-    
-}
+  
+    res.json(proyecto);
+};
 
 const editarProyecto = async (req, res) => {
 
--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -32,28 +32,6 @@ const agregarTarea = async (req, res) => {
 
 
 const obtenerTarea = async (req, res) => {
-    const { id } = req.params;
-
-    const tarea = await Tarea.findById(id).populate('proyecto')
-    
-    if (!tarea) {
-      const error = new Error("Tarea No Encontrada");
-      return res.status(404).json({ msg: error.message });
-    }
-
-    //  si tratan de ver una tarea que no haya creado no se muestra
-    if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()){
-      const error = new Error("Acción no Válida");
-      return res.status(403).json({ msg: error.message });
-    }
-
-    res.json(tarea)
-
-}
-
-
-const actualizarTarea = async (req, res) => {
-
   const { id } = req.params;
 
   const tarea = await Tarea.findById(id).populate("proyecto");
@@ -68,20 +46,37 @@ const actualizarTarea = async (req, res) => {
     return res.status(403).json({ msg: error.message });
   }
 
-  tarea.nombre = req.body.nombre || tarea.nombre
-  tarea.descripcion = req.body.descripcion || tarea.descripcion
-  tarea.prioridad = req.body.prioridad || tarea.prioridad
-  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega
+  res.json(tarea);
+};
+
+
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  tarea.nombre = req.body.nombre || tarea.nombre;
+  tarea.descripcion = req.body.descripcion || tarea.descripcion;
+  tarea.prioridad = req.body.prioridad || tarea.prioridad;
+  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
 
   try {
-    const tareaAlmacenada = await tarea.save()
-    res.json(tareaAlmacenada)
-
+    const tareaAlmacenada = await tarea.save();
+    res.json(tareaAlmacenada);
   } catch (error) {
     console.log(error);
   }
-
-}
+};
 
 
 const eliminarTarea = async (req, res) => {
