05595d50c95953a3763ab941fb060042a595ba14
Jair <jairaceves56@gmail.com>
2023-10-30 10:44:32 -0600

    Mandamos las CORS sin ser Arreglo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ dotenv.config()
 conectarDB()
 
 // Configurar CORS
-const whiteList = [process.env.FRONTEND_URL]
+const whiteList = process.env.FRONTEND_URL
 
 const corsOptions = {
     origin: function(origin, callback){
